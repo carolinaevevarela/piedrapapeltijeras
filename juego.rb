10136ea7f036a7c08f180f90086fd197f06edc9c
Carolina <carolinaevevarela@gmail.com>
2019-04-03 21:25:19 -0300

    segunda actualización

--- juego.rb
+++ juego.rb
@@ -1,39 +1,38 @@
 usuario = ARGV[0]
 pc = rand(3)
-puts pc
 
-if pc == 0
-  computador = "piedra"
-elsif pc == 1
-  computador = "papel"
-elsif pc == 2
-  computador = "tijeras"
+if usuario == "piedra" || usuario == "papel" || usuario == "tijeras"
+    if pc == 0
+      computador = "piedra"
+    elsif pc == 1
+      computador = "papel"
+    elsif pc == 2
+      computador = "tijeras"
+    end
+
+    if usuario == "piedra" && computador == "piedra"
+      puts "Empataste, pc eligió #{computador}"
+    elsif usuario == "piedra" && computador == "papel"
+      puts "Perdiste, pc eligió #{computador}"
+    elsif usuario == "piedra" && computador == "tijeras"
+      puts "Ganaste, pc eligió #{computador}"
+    end
+
+    if usuario == "papel" && computador == "piedra"
+      puts "Ganaste, pc eligió #{computador}"
+    elsif usuario == "papel" && computador == "papel"
+      puts "Empataste, pc eligió #{computador}"
+    elsif usuario == "papel" && computador == "tijeras"
+      puts "Perdiste, pc eligió #{computador}"
+    end
+
+    if usuario == "tijeras" && computador == "piedra"
+      puts "Perdiste, pc eligió #{computador}"
+    elsif usuario == "tijeras" && computador == "papel"
+      puts "Ganaste, pc eligió #{computador}"
+    elsif usuario == "tijeras" && computador == "tijeras"
+      puts "Empataste, pc eligió #{computador}"
+    end
+else
+  puts "Ingresa piedra, papel o tijeras"
 end
-
-if usuario == "piedra" && computador == "piedra"
-  puts "Empataste"
-elsif usuario == "piedra" && computador == "papel"
-  puts "Perdiste"
-elsif usuario == "piedra" && computador == "tijeras"
-  puts "Ganaste"
-end
-
-if usuario == "papel" && computador == "piedra"
-  puts "Ganaste"
-elsif usuario == "papel" && computador == "papel"
-  puts "Empataste"
-elsif usuario == "papel" && computador == "tijeras"
-  puts "Perdiste"
-end
-
-if usuario == "tijeras" && computador == "piedra"
-  puts "Perdiste"
-elsif usuario == "tijeras" && computador == "papel"
-  puts "Ganaste"
-elsif usuario == "tijeras" && computador == "tijeras"
-  puts "Empataste"
-end
-
-#else
-  #puts "Ingresa piedra, papel o tijeras"
-#end
